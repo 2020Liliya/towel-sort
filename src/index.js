cfa5d13a981b123f1a7636a83f8f48e35442db43
Lily Trofimova <trofimovaliliy4@gmail.com>
2022-08-12 17:46:15 +0700

    update: towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined) {
+        return [];
+    } else {
+        const array = matrix.reduce((accumulator, currentValue, i) => {
+            return accumulator.concat((!(i % 2) ? currentValue : currentValue.reverse()));
+        }, []);
+        return array;
+    }
+
 }
